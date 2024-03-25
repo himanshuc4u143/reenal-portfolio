be522c45e7f8ee6147194c9f2c40b1effaca4167
himanshuc4u143 <devendrachitnis25@gmail.com>
2024-03-25 17:04:29 +0530

    sample commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,7 @@ function App() {
   return (
     <div className="App" >
       <div className='nav'><Navbar></Navbar></div>
-      <div className='content'>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='content'>          
       </div>
     </div>
   );
